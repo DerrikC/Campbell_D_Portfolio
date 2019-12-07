8fe8712a9332d0e8a4776758a6e6974f3cd96e02
Derrik Campbell <derrikcampbell@hotmail.com>
2019-12-07 14:27:37 -0500

    trying to fix merge error, too many open modules

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const hbs = require('hbs');
 
 // set the port
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
 
 const app = express();
@@ -103,9 +103,9 @@ app.use((err, req, res, next) => {            //add for custom 404 error message
 
 // app.listen(3000, () => console.log('Listening on port 3000!'));
 
-app.listen(port, () => {
-    console.log(`app is running on ${port}`);
-})
+// app.listen(port, () => {
+//     console.log(`app is running on ${port}`);
+// })
 
 // app.listen(3000);
 
