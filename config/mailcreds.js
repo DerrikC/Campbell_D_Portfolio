6579f0ca0e8b8b131c0027b2388afc115ef6fa7c
Derrik Campbell <derrikcampbell@hotmail.com>
2019-12-12 02:00:54 -0500

    fixing data

--- config/mailcreds.js
+++ config/mailcreds.js
@@ -2,7 +2,7 @@ const mailcreds = {
     host: 'us-cdbr-iron-east-05.cleardb.net',
     service: 'gmail',
     user: 'xstiinct@gmail.com', // your email (gmail) goes here, don't use mine
-    pass: 'campbell15' // see above
+    // pass: '' // see above
 }
 
 module.exports = mailcreds;
